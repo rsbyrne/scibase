3ad912135a2a6020cd85be6b9884821d4a90b3ef
Rohan S Byrne <rohan.byrne@gmail.com>
2020-05-04 14:53:36 +1000

    Added Docker sibling support.

--- it.sh
+++ it.sh
@@ -2,4 +2,5 @@
 MOUNTFROM=$PWD
 MOUNTTO='/home/morpheus/workspace/mount'
 IMAGE='rsbyrne/scibase'
-docker run -v $MOUNTFROM:$MOUNTTO -it --shm-size 2g $IMAGE bash
+SOCK='/var/run/docker.sock'
+docker run -v $MOUNTFROM:$MOUNTTO -v $SOCK:$SOCK -it --shm-size 2g $IMAGE bash
--- nb.sh
+++ nb.sh
@@ -2,5 +2,6 @@
 MOUNTFROM=$PWD
 MOUNTTO='/home/morpheus/workspace/mount'
 IMAGE='rsbyrne/scibase'
-docker run -v $MOUNTFROM:$MOUNTTO --shm-size 2g -p 8888:8888 $IMAGE \
+SOCK='/var/run/docker.sock'
+docker run -v $MOUNTFROM:$MOUNTTO -v $SOCK:$SOCK --shm-size 2g -p 8888:8888 $IMAGE \
   jupyter notebook --no-browser --allow-root --port=8888 --ip='0.0.0.0'
